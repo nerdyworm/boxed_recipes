423af8eb57403129eb68d1d69a0fc18abb03852f
Benjamin Rhodes <ben@nerdyworm.co>
2011-03-10 16:12:04 -0500

    adding ngninx, unicorn

--- config/dna.rb
+++ config/dna.rb
@@ -8,14 +8,14 @@ dna = {
   # users no worky...
   :users =>  {
     :benjamin => {
-      :password => "$1$AMlUfrlU$.OO5fUpKsjXjEsLad2bE7."
+      :password => ""
     },
 
     :ruby => { }
   },
 
   :ssh_keys => {
-    :benjamin => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwTY+mCZUxGr1aIRueEmkuu1wHRFPkkqiNPPflkPMMr8gXS8QVZz+q8DmSD/FU+UesXBMa3k9xVij0K6VsMVR2Sn3WJTcIwg5fKnq8Sp4wSdtcpv5bexRHNCOXPov8SJehYrITcBKOdf7rSjh4pVoOyHtHwNDzHGZ0EIcxbyiZYBTuAoDoKdrsqG0SisAsXe+lRZmCx5KaymAkGh4XLKoUtxkaGUGh0u9M0d9V3c3MULcx5IeymuFYm91cUuUPrjkarTzDlqFlocvXzaGAxN8qWlDSS/bInpLnTmRNetrXcQB6mOl+ym56tNoigCqxBw+Lgf7mmAa3OiHDFUQhxBPYQ== benjamin@kenpachi-ubuntu"
+    :benjamin => ""
   },
 
   :packages => [
